be7f45a97a2a004de5fa5bc06d06318efcb2e9c5
Subhajit Mondal <subhajit25mondal@gmail.com>
2018-10-31 02:59:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -56,18 +56,6 @@
     <?php
     
     
-   if(isset($_POST['submit'])){
-    
-    $get_value = $_POST['input'];
-    
-    
- $con = mysqli_connect("localhost","root","");
-mysqli_select_db($con, "url");
-
-$a=array("rfj","acx","auk","znd","dzi","plj","ygh","rcv","bcx","zaq","tfg","tgf");
-$random=array_rand($a,2);
-
-$per=$a[$random[0]];
 
 
 
@@ -93,34 +81,6 @@ fwrite($myfile, $url2);
 fclose($myfile);                                                
 
 
-
-
-                                                                                
-
-$b=$_SERVER['REMOTE_ADDR'];
-
-
-date_default_timezone_set('Asia/Kolkata');
-
-$timestamp = time();
-$date_time = date("d-m-Y (D) H:i:s", $timestamp);
-
-
-                                                                                  
-
-
-
-
-
-$tttt=$largestNumber+1;
-$ttt=$per.($largestNumber+1);
-
-
-$insert_query = "insert into main (id, url, link, ip, time) values ('$tttt','$url','$ttt','$b','$date_time')";
-
-
-
-$res=mysqli_query($con, $insert_query);
 
 
 echo "<code id=\"mycode\"> /localhost/url/$ttt </code>";
@@ -163,36 +123,6 @@ URL Compressor is a service that takes long URLs and squeezes them into fewer ch
 <div class="copyright text-center">
 Designed and Developed by Subhajit Mondal </div>
 </div>
-</footer>
-
- 
-    
-    
-    
-    
-    
-    <script src="./index_files/jquery.min.js.download"></script>
-     <script src="./index_files/wow.min.js.download"></script>
-    <script src="./index_files/jquery.particleground.min.js.download"></script>
-<script src="./index_files/front.js.download"></script><script src="./index_files/app.js.download"></script>
-</body></html>
-
-
-
-
-
-<style>
-
-code {
-		border-radius: 8px;
-		border: solid 0.5px;
-		font-family: "Courier New", monospace;
-		font-size: 1.7em;
-		margin: 0 0.25em;
-		padding: 0.25em 0.65em;
-        color: black;
-	}
-
 
 
 
